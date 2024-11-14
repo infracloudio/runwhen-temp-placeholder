9eb4b6b98f4d629c34991680ee59a200f2898558
saurabh3460 <saurabh3460@gmail.com>
2024-11-14 16:29:23 +0530

    variablized scripts

--- create_ebs.sh
+++ create_ebs.sh
@@ -1,20 +1,27 @@
 #!/bin/bash
 
 # GIT_TLD=`git rev-parse --show-toplevel`
+REGION="us-west-2"
+AZ="${REGION}b"
+EBS_VOLUME_NAME="ebs-test"
+EBS_VOLUME_SIZE="1"
 
 
 EBS_VOLUME_EXISTS=$(
     aws ec2 describe-volumes \
-    --filters Name=tag:Name,Values=ebs-test \
+    --filters Name=tag:Name,Values=$EBS_VOLUME_NAME \
     --query 'Volumes[0].VolumeId' \
-    --region="us-west-2" \
+    --region=$REGION \
     --output text \
     --no-cli-pager
 )
 
 
 if [[ "$EBS_VOLUME_EXISTS" == "None" ]]; then
-    aws ec2 create-volume --region="us-west-2" --availability-zone="us-west-2a" --size="1" --no-encrypted  --tag-specifications 'ResourceType=volume,Tags=[{Key=Name,Value=ebs-test}]'
+    aws ec2 create-volume --region=$REGION \
+    --availability-zone=$AZ --size=$EBS_VOLUME_SIZE \
+    --tag-specifications "ResourceType=volume,Tags=[{Key=Name,Value=$EBS_VOLUME_NAME}]" \
+    --output text --no-encrypted --no-cli-pager
 else 
     echo "$EBS_VOLUME_EXISTS ebs already exists"
 fi
--- delete_ebs.sh
+++ delete_ebs.sh
@@ -2,18 +2,21 @@
 
 # GIT_TLD=`git rev-parse --show-toplevel`
 
+REGION="us-west-2"
+EBS_VOLUME_NAME="ebs-test"
+
 EBS_VOLUME_EXISTS=$(
     aws ec2 describe-volumes \
-    --filters Name=tag:Name,Values=ebs-test \
+    --filters Name=tag:Name,Values=$EBS_VOLUME_NAME \
     --query 'Volumes[0].VolumeId' \
-    --region="us-west-2" \
+    --region=$REGION \
     --output text \
     --no-cli-pager
 )
 
 if [[ "$EBS_VOLUME_EXISTS" != "None" ]]; then
     echo "Deleting EBS volume $EBS_VOLUME_EXISTS..."
-    aws ec2 delete-volume --volume-id "$EBS_VOLUME_EXISTS" --region="us-west-2" --no-cli-pager
+    aws ec2 delete-volume --volume-id "$EBS_VOLUME_EXISTS" --region=$REGION --no-cli-pager
 else 
     echo "No EBS volume with tag Name=ebs-test exists."
 fi
